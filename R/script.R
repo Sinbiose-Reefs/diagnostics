639afbaf81d3ede46c9c3bd87ebaa67078ce65e4
andreluza <luza.andre@gmail.com>
2022-09-29 11:40:07 -0300

    trait_space_trends_in_time

--- R/script.R
+++ R/script.R
@@ -1523,6 +1523,187 @@ grid.arrange(plotA2,
 dev.off()
 
 
+# --------------------------------
+# exploring trait space over time
+
+require(sp)
+
+TS_time <- lapply (seq (min (fisheries_wtrait$Year), max(fisheries_wtrait$Year)), function (t) {
+  
+  # define year
+  year <- t
+  
+  # choose the dataset of one specific year
+  chosen_set <- fisheries_wtrait[which(fisheries_wtrait$Year == year),]
+  
+  # filter genus
+  # points proportional to the catch
+  catch_year_genus <- chosen_set %>%
+    
+    group_by(Genus_match,Year) %>% 
+    
+    summarize(sum_catch=sum(CatchAmount_t,na.rm=T),
+    ) 
+  
+  
+  # 5% threshold to consider bycatch
+  bycatch_year <-sum(catch_year_genus$sum_catch,na.rm=T)*percentage_for_bycatch
+  catch_year_genus<- catch_year_genus[which(catch_year_genus$sum_catch >bycatch_year),]
+  
+  
+  # size
+  all_year <- data.frame (all , 
+                          catch_year_genus [match (all$sp, 
+                                                   catch_year_genus$Genus_match), 
+                                            "sum_catch"])
+  
+  # rm NAs
+  all_year<-all_year[is.na(all_year$sum_catch) != T,]
+  
+  
+  #year convex hull
+  a_year <- all_year [chull(all_year[,1:2], y = NULL),] # its convex hull
+  
+  # quantifying reduction in functional space
+  # https://chitchatr.wordpress.com/2015/01/23/calculating-the-area-of-a-convex-hull/
+  chull.poly.complete <- Polygon(a_overall[,1:2], hole=F)
+  chull.area.complete <- chull.poly.complete@area
+
+
+
+  # year trait space
+  chull.poly.year <- Polygon(a_year[,1:2], hole=F)
+  chull.area.year <- chull.poly.year@area
+
+  # ratio
+  df_res <- data.frame (completeTS = chull.area.complete,
+                        yearTS =chull.area.year,
+                        ratioTS=chull.area.year/chull.area.complete,
+                        year = t)
+
+})
+
+
+# plot 
+TS_time_df <- do.call (rbind, TS_time)
+
+# BR trend
+BR_trend_TS<- ggplot (TS_time_df, aes (x=year,y= ratioTS)) + 
+  geom_point() + 
+  geom_smooth() + 
+  ylab ("Year trait space area / Complete trait space area") + 
+  theme_classic()
+
+
+
+# ---------------------------------------------------------------
+# per region
+
+TS_time_region <- lapply (unique(fisheries_wtrait$Region), function (r) # each region
+  
+  lapply (seq (min (fisheries_wtrait$Year), max(fisheries_wtrait$Year)), function (t) { # each year
+  
+  # define year
+  year <- t
+  
+  # choose the dataset of one specific year
+  chosen_set <- fisheries_wtrait[which(fisheries_wtrait$Region == r & fisheries_wtrait$Year == year),]
+  
+  # filter genus
+  # points proportional to the catch
+  catch_year_genus <- chosen_set %>%
+    
+    group_by(Genus_match,Year) %>% 
+    
+    summarize(sum_catch=sum(CatchAmount_t,na.rm=T),
+    ) 
+  
+  
+  # 5% threshold to consider bycatch
+  bycatch_year <-sum(catch_year_genus$sum_catch,na.rm=T)*percentage_for_bycatch
+  catch_year_genus<- catch_year_genus[which(catch_year_genus$sum_catch >bycatch_year),]
+  
+  
+  # size
+  all_year <- data.frame (all , 
+                          catch_year_genus [match (all$sp, 
+                                                   catch_year_genus$Genus_match), 
+                                            "sum_catch"])
+  
+  # rm NAs
+  all_year<-all_year[is.na(all_year$sum_catch) != T,]
+  
+  
+  #year convex hull
+  a_year <- all_year [chull(all_year[,1:2], y = NULL),] # its convex hull
+  
+  # quantifying reduction in functional space
+  # https://chitchatr.wordpress.com/2015/01/23/calculating-the-area-of-a-convex-hull/
+  chull.poly.complete <- Polygon(a_overall[,1:2], hole=F)
+  chull.area.complete <- chull.poly.complete@area
+  
+  
+  
+  # year trait space
+  chull.poly.year <- Polygon(a_year[,1:2], hole=F)
+  chull.area.year <- chull.poly.year@area
+  
+  # ratio
+  df_res <- data.frame (completeTS = chull.area.complete,
+                        yearTS =chull.area.year,
+                        ratioTS=chull.area.year/chull.area.complete,
+                        year = t,
+                        region = r)
+  
+  }) # close year
+) # close region
+
+# plot 
+TS_time_region_df <- lapply (TS_time_region, function (r) 
+  
+  do.call (rbind, r)
+)
+# melt the list once again  
+TS_time_region_df<- do.call(rbind, TS_time_region_df)
+# discreticing year
+TS_time_region_df<-TS_time_region_df %>% 
+  mutate(year_binned = cut_number(year, n=6))
+
+
+# BR trend
+
+region_trend_TS<-ggplot (TS_time_region_df, aes (x=year,y= ratioTS)) + 
+  geom_point() + 
+  geom_smooth() + 
+  ylab ("Year trait space area / Complete trait space area") + 
+  theme_classic() + 
+  facet_wrap(~region)
+
+
+grid.arrange (
+  BR_trend_TS + theme (axis.title = element_blank()),
+  region_trend_TS+ theme (axis.title = element_blank()),
+  nrow=2, ncol=2,
+  layout_matrix = rbind (c(1,1),
+                         c(2,2),
+                         c(2,2),
+                         c(2,2))
+  )
+
+
+# boxplot 
+
+region_trend_TS_discrete<-ggplot (TS_time_region_df, aes (x=year_binned,y= ratioTS)) + 
+  geom_boxplot()+
+  ylab ("Year trait space area / Complete trait space area") + 
+  theme_classic() + 
+  theme(axis.text.x = element_text(angle=45))+
+  facet_wrap(~region)
+region_trend_TS_discrete
+
+
+
+
 # -----------------------------
 
 
@@ -1731,347 +1912,6 @@ trend_sel_sp_region_cart+theme (axis.title.x = element_blank(),
                                               legend.text = element_text(size=8))
 
 dev.off()
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ------------------------------------------
-
-
-# (not in this paper)
-
-
-
-
-
-
-
-# ==============================================================
-
-# Are fisheries getting nutritionally poorer over time?
-# zinc
-
-zinc_genus <- tapply (fisheries_wtrait$Zinc_mu,
-                       list (fisheries_wtrait$Genus_match,
-                             fisheries_wtrait$Region),
-                       mean,na.rm=T)
-
-# iron
-iron_genus <- tapply (fisheries_wtrait$Iron_mu,
-                      list (fisheries_wtrait$Genus_match,
-                            fisheries_wtrait$Region),
-                      mean,na.rm=T)
-
-# omega 3
-omega_genus <- tapply (fisheries_wtrait$Omega_3_mu,
-                      list (fisheries_wtrait$Genus_match,
-                            fisheries_wtrait$Region),
-                      mean,na.rm=T)
-
-# protein
-#protein_genus <- tapply (fisheries_wtrait$Protein_mu,
-#                      list (fisheries_wtrait$Genus_match,
-#                            fisheries_wtrait$Region),
-#                      mean,na.rm=T)
-
-# calcium
-calcium_genus <- tapply (fisheries_wtrait$Calcium_mu,
-                         list (fisheries_wtrait$Genus_match,
-                               fisheries_wtrait$Region),
-                         mean,na.rm=T)
-
-# selenium
-selenium_genus <- tapply (fisheries_wtrait$Selenium_mu,
-                         list (fisheries_wtrait$Genus_match,
-                               fisheries_wtrait$Region),
-                         mean,na.rm=T)
-
-# vitA
-vitA_genus <- tapply (fisheries_wtrait$Vitamin_A_mu,
-                          list (fisheries_wtrait$Genus_match,
-                                fisheries_wtrait$Region),
-                          mean,na.rm=T)
-
-# list of nutrient data
-nutrient_data <- list (zinc_genus,iron_genus,omega_genus,
-                       #protein_genus,
-                       calcium_genus,selenium_genus , vitA_genus)
-
-# removing bycatch
-nutrient_data <- lapply (nutrient_data, function (i)
-
-  i [which(rownames(i) %in% notbycatch),]
-)
-
-# match with catch amount data
-#nutrient_data <- lapply (nutrient_data, function (i)
-#  
-#  i <- cbind (i, amount = catch_year_genus [match (rownames(i), 
-#                           catch_year_genus$Genus_match), 
-#                    "sum_catch"])
-#  
-#)
-
-# extract data
-fish_year_nutrition <- lapply (fish_year, function (i) 
-  do.call(rbind, lapply (nutrient_data, function (k)  { # bind nutrient data
-  
-  
-  fish_year_genus <- tapply (i$CatchAmount_t,
-                             list (i$Genus_match,
-                                   i$Region),
-                             sum)
-  
-  # Mean size among the most frequent nsp_choose 
-  # across regions
-  nut_reg <- do.call(cbind,   # melt nutrient data per region
-    
-    lapply (seq (1,ncol (fish_year_genus)), function (reg) {
-  
-        nut_collect_species <- names (fish_year_genus[,reg])[order(fish_year_genus[,reg],decreasing=T)][1:nsp_choose]
-        nut_collect_species <- k[which(names (k[,reg]) %in% nut_collect_species),reg]
-        mean_nut_collect_species<- mean(nut_collect_species,na.rm=T)
-        mean_nut_collect_species
-  }))
-  colnames(nut_reg) <- colnames(fish_year_genus)
-  ; # return
-  nut_reg
-  
-  
-})))
-# name rows
-fish_year_nutrition<- lapply (fish_year_nutrition, function (i) {
-  rownames (i)<- c("Zinc","Iron", "Omega-3", 
-                   #"Protein",
-                   "Calcium","Selenium","Vitamin-A")
-  ;
-  i
-})
-# name year
-names (fish_year_nutrition) <- names(fish_year)
-# melt
-fish_year_nutrition <-( do.call(rbind.data.frame, fish_year_nutrition))
-fish_year_nutrition$year <- sapply (strsplit ( rownames(fish_year_nutrition), "\\."), "[",1)
-fish_year_nutrition$nutrient <- sapply (strsplit ( rownames(fish_year_nutrition), "\\."), "[",2)
-# melt to fit ggplot format
-fish_year_nutrition<- melt(fish_year_nutrition,id.var=c("year", "nutrient"))
-
-
-# plot 
-
-plot_nut <- ggplot (fish_year_nutrition, aes (x=year, 
-                                  y=value,
-                                  group=variable,
-                                  colour=variable)) + 
-  geom_point(alpha=0.2)+
-  facet_wrap(~nutrient,scales = "free_y",ncol=7)+
-  geom_smooth(method = "gam",
-              formula = y ~ s(x, bs = "cs",k=4))+
-  scale_x_discrete(
-    
-    breaks = seq(min(fish_year_nutrition$year), 
-                 max(fish_year_nutrition$year), by = 20),
-    
-  ) + 
-  ylab ("Content of nutrients") + 
-  xlab("Year")+
-  theme_classic() + 
-  theme(axis.text = element_text(size=5),
-        axis.title = element_text(size=13),
-        strip.text = element_text(face="bold"),
-        strip.text.x = element_text(size = 10, color = "black", 
-                                    face = "bold"),
-        strip.background = element_rect(color="black", 
-                                        fill="gray60",
-                                        size=1.5, linetype="solid"
-        )) + 
-  scale_colour_viridis_d(name = "Region", begin=0,end=1)
-  
-
-
-plot_nut
-
-
-
-# ordination to show the nutrition content of fish genus
-genus_nutrient_composition <- lapply (nutrient_data, function (i) 
-  
-          apply (i,1, mean,na.rm=T)
-          
-)
-genus_nutrient_composition <- do.call(cbind,genus_nutrient_composition)
-colnames(genus_nutrient_composition) <- c("Zinc","Iron", "Omega-3",
-                                          #"Protein",
-                                          "Calcium","Selenium","Vitamin-A")
-
-
-
-# removing missing data
-genus_nutrient_composition<-genus_nutrient_composition[which(rowSums(genus_nutrient_composition>0,
-                                                                     na.rm=T)>0),]
-
-genus_data <- rownames(genus_nutrient_composition)
-
-
-# standardize values
-genus_nutrient_composition<-apply (genus_nutrient_composition, 2,scale)
-rownames(genus_nutrient_composition) <- genus_data
-
-
-# weigth nutrient composition by catch amount
-catch_amount_genus <- tapply (fisheries_wtrait$CatchAmount_t,
-                          list (fisheries_wtrait$Genus_match),
-                      sum,na.rm=T)
-# removing missing data
-catch_amount_genus<-catch_amount_genus[which(rownames (catch_amount_genus) %in% genus_data)]
-catch_amount_genus[order(catch_amount_genus,decreasing=T)]
-
-# standardize values
-catch_amount_genus<-decostand ((catch_amount_genus),method= "standardize")
-catch_amount_genus[order(catch_amount_genus)]
-
-
-# distance matrix
-dist_nut <- vegdist (genus_nutrient_composition*catch_amount_genus[,1],  # weighted matrix
-                     "euclidean")
-
-# ordination
-pcoa_nut <- pcoa(dist_nut)
-
-
-# variance explained by the first and second axes
-Exp_axis1<-pcoa_nut$values$Eigenvalues[1]/sum(pcoa_nut$values$Eigenvalues)*100
-Exp_axis2<-pcoa_nut$values$Eigenvalues[2]/sum(pcoa_nut$values$Eigenvalues)*100
-
-# data
-
-#pcoa_fish_year <- melt (pcoa_fish_year)
-pcoa_nut <- data.frame (pcoa_nut$vectors[,1:2],
-                   sp = rownames(pcoa_nut$vectors))
-
-
-
-# corelation to project nutrients
-correlation_nut <-data.frame( cor (genus_nutrient_composition,
-                                   pcoa_nut[,1:2],
-                               method = "pearson"))
-correlation_nut$nutrient <- rownames(correlation_nut)
-# ordination
-# help here
-# https://ggplot2.tidyverse.org/reference/geom_path.html
-ordination_nut<-ggplot(data=pcoa_nut,
-                    aes(x=Axis.1,y=Axis.2)) + 
-  geom_point(colour="black",alpha=0.5,stroke=1.5,
-             shape=1,size=3) + # add the point markers
-  #geom_path(aes(colour=as.numeric(sp)),alpha=0.5)+
-  
-  geom_text_repel(aes(label=sp ),size=3,vjust=1,
-                  max.overlaps = 50)+#ifelse(Axis.1>1.5 |
-                            #   Axis.1 < -1 |
-                            #   Axis.2 >1 |
-                            #   Axis.2 < -2,as.character(sp),'')),
-            #size=2.5,vjust=1) +
-  #geom_path(aess(group=year)) +# add the site labels
-  #scale_colour_manual(values=c("A" = "red", "B" = "blue")) +
-  coord_equal() +
-  theme_bw() +
-  theme(legend.position = "none") + 
-  xlab (paste ("Axis 1 (", round(Exp_axis1,2), "%)",sep="")) +
-  ylab (paste ("Axis 2 (", round(Exp_axis2,2), "%)",sep=""))+
-  xlim (c(-4.75,3)) + 
-  ylim (c(-2.5,2)) 
-
-
-
-
-# project genus names
-ordination_nut<-ordination_nut + 
-  
-                    geom_text(data = correlation_nut,
-                                           aes (x=Axis.1*5,
-                                                y = Axis.2*5,
-                                                label = (nutrient)),
-                                           size=5,fontface = "italic",
-                                           colour="#5BB318",
-                                           max.overlaps = 100)  
-# arrows
-ordination_nut <- ordination_nut + geom_segment(aes(x = 0, y = 0, 
-                                  xend = correlation_nut[1,1]*4, 
-                                  yend = correlation_nut[1,2]*4),size = 1,
-                              color="#2B7A0B",alpha=0.05,
-                              arrow = arrow(length = unit(.35, "cm"))) + 
-   geom_segment(aes(x = 0, y = 0, 
-                     xend = correlation_nut[2,1]*4, 
-                     yend = correlation_nut[2,2]*4),size = 1,
-                 color="#2B7A0B",alpha=0.05,
-                 arrow = arrow(length = unit(.35, "cm"))) + 
-  geom_segment(aes(x = 0, y = 0, 
-                   xend = correlation_nut[3,1]*4, 
-                   yend = correlation_nut[3,2]*4),size = 1,
-               color="#2B7A0B",alpha=0.05,
-               arrow = arrow(length = unit(.35, "cm"))) + 
-  geom_segment(aes(x = 0, y = 0, 
-                   xend = correlation_nut[4,1]*4, 
-                   yend = correlation_nut[4,2]*4),size = 1,
-               color="#2B7A0B",alpha=0.05,
-               arrow = arrow(length = unit(.35, "cm"))) + 
-  geom_segment(aes(x = 0, y = 0, 
-                   xend = correlation_nut[5,2]*4, 
-                   yend = correlation_nut[5,2]*4),size = 1,
-               color="#2B7A0B",alpha=0.05,
-               arrow = arrow(length = unit(.35, "cm"))) + 
-  geom_segment(aes(x = 0, y = 0, 
-                   xend = correlation_nut[6,1]*4, 
-                   yend = correlation_nut[6,2]*4),size = 1,
-               color="#2B7A0B",alpha=0.05,
-               arrow = arrow(length = unit(.35, "cm")))
-
-
-
-# arrange
-
-
-pdf (here ("output", "nutrients.pdf"),height=5,width=9)
-
-composition2<-grid.arrange(ordination_nut,
-             plot_nut,
-             ncol=5,nrow=5,
-             layout_matrix = rbind (c (1,1,1,1,1),
-                                    c (1,1,1,1,1),
-                                    c (1,1,1,1,1),
-                                    c (2,2,2,2,2),
-                                    c (2,2,2,2,2)))
-
-
-
-dev.off()
-
-
-
-# ======================
-
-# 
-#source (here ("R", "function_poncho.R"))
-#
-#pdf("year.pdf")
-#poncho (log(year_composition+1),
-#        gradient = seq(1,nrow(year_composition)),
-#        col = as.numeric(as.factor(fisheries_wtrait$Diet_2012[match (colnames(year_composition),
-#                                                fisheries_wtrait$Genus)])),lty = 0)
-#
-#dev.off()
-#
-
-
 
 
 
